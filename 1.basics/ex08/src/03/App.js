69dcc3d356e7cbb14209fd922a251eee4a235e4f
awlals <awlals616@gmail.com>
2023-03-08 11:29:56 +0900

    jsx tutorials

---
+++ 1.basics/ex08/src/03/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
+
+function App() {
+
+  return (
+    <div id="App">
+       <Header/>
+        <Content/>
+    </div>
+  );
+}
+
+export default App;
