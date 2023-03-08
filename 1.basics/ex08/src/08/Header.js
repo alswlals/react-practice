1dbc91b2a3efd036a2fc62ad7339da3d7d7044c1
awlals <awlals616@gmail.com>
2023-03-08 15:45:47 +0900

    component

---
+++ 1.basics/ex08/src/08/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Header(){
+    return (
+        <h2>08</h2>
+    )
+}
+export default Header;
---
+++ 2.component/ex01/src/01/FoodListItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+function FoodListItem({name, count}) {
+  return (
+    <li>{name}:{count}</li>
+  )
+}
+export default FoodListItem;
