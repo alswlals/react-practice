35a4cde261775808025592800706af08b6a41519
awlals <awlals616@gmail.com>
2023-03-09 14:50:06 +0900

    kanbanboard #2

--- kanbanboard/frontend/src/App.js
+++ kanbanboard/frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import './assets/css/App.css'
+import KanbanBoard from './KanbanBoard';
 export default function App(props) {
    
     return (
-        <div id='App'>
-            <h1>Kanbanboard App</h1>
+        <div id='App'className={'App'} >
+            <KanbanBoard />
         </div>
     );
 } 
