c87397fa2dfb94816c6f5e6e0449ac7a97bd1220
awlals <awlals616@gmail.com>
2023-03-09 15:39:19 +0900

    EmailList #1

---
+++ emaillist/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './assets/css/App.css';
+// import Emaillist from './Emaillist';
+export default function App(props) {
+    return (
+        <div id='App'className={'App'} >
+            {/* <Emaillist /> */}
+            <h1>Emaillist</h1>
+        </div>
+    );
+} 
+
