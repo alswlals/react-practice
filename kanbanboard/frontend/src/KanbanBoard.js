85f75027c58b0679b1755d3562ff85cfe99856b2
awlals <awlals616@gmail.com>
2023-03-14 17:25:25 +0900

    kanbanboard

--- kanbanboard/frontend/src/KanbanBoard.js
+++ kanbanboard/frontend/src/KanbanBoard.js
@@ -2,18 +2,31 @@ import React, {useState} from 'react';
 import styles from './assets/css/KanbanBoard.css';
 import data from './assets/json/data.json';
 import CardList from './CardList';
+import update from 'react-addons-update';
 
 const KanbanBoard = () => {
     const [cards, setCards] = useState(data)
 
     const changeTaskDone = function(cardNo, taskNo, done){
-        console.log(cardNo, taskNo, done);
+        // console.log(cardNo, taskNo, done);
         const cardIndex = cards.findIndex(card => card.no === cardNo);
         const taskIndex = cards[cardIndex].tasks.findIndex(task=> task.no === taskNo);
-        console.log(cardIndex, taskIndex);
+        // console.log(cardIndex, taskIndex);
        
-        cards[cardIndex].tasks[taskIndex].done = done;
-        setCards(cards);
+        // cards[cardIndex].tasks[taskIndex].done = done;
+        const newCards = update(cards, {
+            [cardIndex]: {  // 변수를 뽑아놓은 거기 때문에 저렇게 써줘야 함 [cardIndex] -> 2. /ex06/04.js
+                tasks: {
+                    [taskIndex]: {
+                        done: {
+                            $set: done
+                        }
+                    }
+                }
+            }
+        })
+
+        setCards(newCards);
     }
 
     return (
