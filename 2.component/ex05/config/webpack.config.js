5e79517ee82d75fcb90ffcc45d48c51f2f3ebe25
awlals <awlals616@gmail.com>
2023-03-16 09:52:06 +0900

    webpack

--- 2.component/ex05/config/webpack.config.js
+++ 2.component/ex05/config/webpack.config.js
@@ -4,7 +4,7 @@ const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 module.exports = function(env){
     return {
         mode: "none",
-        entry: path.resolve(`src/index.js`),
+        entry: path.resolve(`src/${env.src}/index.js`),
         output: {
             path: path.resolve('public'),
             filename: 'assets/js/main.js',
