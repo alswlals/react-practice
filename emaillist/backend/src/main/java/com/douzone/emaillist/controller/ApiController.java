644bd8e1cf15e224f6c78f685c7c9fa5d55bc4e1
awlals <awlals616@gmail.com>
2023-03-16 15:59:47 +0900

    emaillist

--- emaillist/backend/src/main/java/com/douzone/emaillist/controller/ApiController.java
+++ emaillist/backend/src/main/java/com/douzone/emaillist/controller/ApiController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,8 +36,8 @@ public class ApiController {
 				.body(JsonResult.success(vo));
 	}
 	
-	@DeleteMapping("/api")
-	public ResponseEntity<JsonResult> delete(@RequestBody Long no){
+	@DeleteMapping("/api/{no}")
+	public ResponseEntity<JsonResult> delete(@PathVariable("no") Long no){
 		emaillistService.delete(no);
 		
 		return ResponseEntity
